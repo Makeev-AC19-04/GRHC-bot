7b4d869ae6dcac7ebdefc742bdd37241133d205c
Makeev-AC19-04 <wane2001@gmail.com>
2022-06-30 16:47:13 +0300

    Создал бота, привязал токен, повторение сообщений

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 
-token = '5589670914:AAGZFaTykB0AelTJFO1T0v63WdAlF-ylMt4' #git check
+token = '5589670914:AAGZFaTykB0AelTJFO1T0v63WdAlF-ylMt4' #git  heck
 
 bot = telebot.TeleBot(token)
 
