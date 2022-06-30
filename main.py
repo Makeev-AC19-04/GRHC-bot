bfe3c84e055394548686d0bc8a6e382144ac4410
Makeev-AC19-04 <wane2001@gmail.com>
2022-06-30 17:06:17 +0300

    Создал бота, привязал токен, повторяет сообщения

---
+++ main.py
@@ -0,0 +1,11 @@
+import telebot
+
+token = '5589670914:AAGZFaTykB0AelTJFO1T0v63WdAlF-ylMt4'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(content_types=['text'])
+def message_handler(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
